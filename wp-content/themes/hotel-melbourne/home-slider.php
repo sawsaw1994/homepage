d0b42bb63c3b1a0f1d8470a15531178308df1771
sawsaw1994 <gnext.sawzinmintun@gmail.com>
2018-11-16 09:37:39 +0900

    change design

--- wp-content/themes/hotel-melbourne/home-slider.php
+++ wp-content/themes/hotel-melbourne/home-slider.php
@@ -8,38 +8,58 @@
 * @filesource   :	wp-content/themes/melbourne/home-slider.php
 */
 ?>
-<!-- Slider Section -->	
-<?php 
-$melbourne_options=hotel_melbourne_theme_default_data(); 
-$slider_setting = wp_parse_args(  get_option( 'melbourne_option', array() ), $melbourne_options ); 
+<!-- Slider Section -->
+<?php
+$melbourne_options=hotel_melbourne_theme_default_data();
+$slider_setting = wp_parse_args(  get_option( 'melbourne_option', array() ), $melbourne_options );
 if($slider_setting['home_banner_enabled'] == 1 ) {
 
 $slideshowSpeed=$slider_setting['slideshowSpeed'];
 ?>
 <script type="text/javascript">
 		jQuery(window).load(function(){
-		  jQuery('.flexslider').flexslider({	
+		  jQuery('.flexslider').flexslider({
 			animation: "slide",
 			animationSpeed: 1500,
 			direction: "vertical",
-			
-			directionNav: true, 
-			prevText: "Previous",          
+
+			directionNav: true,
+			prevText: "Previous",
 			nextText: "Next",
-			controlNav: true,			
-			
-			slideshowSpeed: "<?php echo $slideshowSpeed; ?>",
-			pauseOnHover: true, 
+			controlNav: true,
+
+			slideshowSpeed: "<?php echo $slideshowSpeed - 1000; ?>",
+			pauseOnHover: true,
 			slideshow: true,
 			start: function(slider){
 			  jQuery('body').removeClass('loading');
-			}			
+			}
 		  });
-		  
-		  
+
+
+			$("ul.slides").on('click', "li.flex-prev,li.flex-next", function(){
+        this.changeBackgroundImage();
+    	})
+
+
+
 		});
+		setInterval(function(){
+			this.changeBackgroundImage();
+	  }, 1000);
+
+		function changeBackgroundImage() {
+			$("ul.slides li").each(function(){
+				var srcPath = $('.flex-active-slide').find('img').attr('src');
+				if($(this).hasClass('flex-active-slide')) {
+					$('.wrapper').css({
+	 					 "background": `url(${srcPath}) no-repeat fixed 0 0 / cover rgba(0, 0, 0, 0)`
+	 				 });
+				}
+			});
+		}
 </script>
-<!--Flex Slider-->	
+<!--Flex Slider-->
 	<div class="flexslider">
         <div class="flex-viewport">
 			<ul class="slides">
@@ -47,7 +67,7 @@ $slideshowSpeed=$slider_setting['slideshowSpeed'];
 				<?php if($slider_setting['slider_image_one'] !='') { ?>
 				<img src="<?php echo esc_url($slider_setting['slider_image_one']); ?>" class="img-responsive" alt="<?php echo esc_attr($slider_setting['slider_image_title_one']); ?>">
 			<?php } ?>
-					
+
 					  <div class="slide-caption text-center">
 					  <?php if($slider_setting['slider_image_title_one'] !='') { ?>
 						<h1 class="animated bounceInLeft"><?php echo esc_attr($slider_setting['slider_image_title_one']); ?>.</b></h1>
@@ -56,15 +76,15 @@ $slideshowSpeed=$slider_setting['slideshowSpeed'];
 						   <?php if($slider_setting['slider_button_text'] !='') { ?>
 						     <a class="main-btn" href="<?php if($slider_setting['slider_image_link'] !='') { echo esc_url($slider_setting['slider_image_link']); } ?>" <?php if($slider_setting['slider_button_tab'] ==1) { echo "target='_blank'"; } ?> ><i class="fa fa-key"></i><?php echo esc_attr($slider_setting['slider_button_text']) ?></a>
 							 <?php } ?>
-						   </div>		
+						   </div>
                     </div>
 				</li>
-					
+
 					<li>
 				<?php if($slider_setting['slider_image_two'] !='') { ?>
 				<img src="<?php echo esc_url($slider_setting['slider_image_two']); ?>" class="img-responsive" alt="<?php echo esc_attr($slider_setting['slider_image_title_two']); ?>">
 			<?php } ?>
-					
+
 					  <div class="slide-caption text-center">
 					  <?php if($slider_setting['slider_image_title_two'] !='') { ?>
 						<h1 class="animated bounceInLeft"><?php echo esc_attr($slider_setting['slider_image_title_two']); ?>.</b></h1>
@@ -73,15 +93,15 @@ $slideshowSpeed=$slider_setting['slideshowSpeed'];
 						   <?php if($slider_setting['slider_button_text'] !='') { ?>
 						     <a class="main-btn" href="<?php if($slider_setting['slider_image_link'] !='') { echo esc_url($slider_setting['slider_image_link']); } ?>" <?php if($slider_setting['slider_button_tab'] ==1) { echo "target='_blank'"; } ?> ><i class="fa fa-key"></i><?php echo esc_attr($slider_setting['slider_button_text']) ?></a>
 							 <?php } ?>
-						   </div>		
+						   </div>
                     </div>
 				</li>
-				
+
 				<li>
 				<?php if($slider_setting['slider_image_three'] !='') { ?>
 				<img src="<?php echo esc_url($slider_setting['slider_image_three']); ?>" class="img-responsive" alt="<?php echo esc_attr($slider_setting['slider_image_title_three']); ?>">
 			<?php } ?>
-					
+
 					  <div class="slide-caption text-center">
 					  <?php if($slider_setting['slider_image_title_one'] !='') { ?>
 						<h1 class="animated bounceInLeft"><?php echo esc_attr($slider_setting['slider_image_title_three']); ?>.</b></h1>
@@ -90,12 +110,12 @@ $slideshowSpeed=$slider_setting['slideshowSpeed'];
 						   <?php if($slider_setting['slider_button_text'] !='') { ?>
 						     <a class="main-btn" href="<?php if($slider_setting['slider_image_link'] !='') { echo esc_url($slider_setting['slider_image_link']); } ?>" <?php if($slider_setting['slider_button_tab'] ==1) { echo "target='_blank'"; } ?> ><i class="fa fa-key"></i><?php echo esc_attr($slider_setting['slider_button_text']) ?></a>
 							 <?php } ?>
-						   </div>		
+						   </div>
                     </div>
 				</li>
 			</ul>
 		</div>
-	</div> 
+	</div>
 	<!--/Flex Slider-->
 <div class="clearfix"></div>
 <?php } ?>
