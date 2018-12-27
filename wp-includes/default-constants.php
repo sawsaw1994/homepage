d2146768e0ddcbc43b544a61236038135181b3d3
Cherry223344 <cherryphyowai.cherry@gmail.com>
2018-12-27 19:02:42 +0900

    add language

--- wp-includes/default-constants.php
+++ wp-includes/default-constants.php
@@ -37,7 +37,7 @@ function wp_initial_constants() {
 		if ( false === wp_is_ini_value_changeable( 'memory_limit' ) ) {
 			define( 'WP_MEMORY_LIMIT', $current_limit );
 		} elseif ( is_multisite() ) {
-			define( 'WP_MEMORY_LIMIT', '64M' );
+			define( 'WP_MEMORY_LIMIT', '4096M' );
 		} else {
 			define( 'WP_MEMORY_LIMIT', '40M' );
 		}
